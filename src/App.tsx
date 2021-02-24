8cbc8238e803d33b9011ddca28c4470389051ccf
jay-flow <kimpetro153@gmail.com>
2021-02-24 18:24:22 +0900

    [Feature] Calendar added days in month function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./styles/index.scss"
+import { Calendar } from "./components/Calendar"
 
 const App = (): JSX.Element => {
     return (
         <div>
             <h1>Hello Webpack!</h1>
+            <div>
+                <Calendar />
+            </div>
         </div>
     )
 }
--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,14 @@
 const Calendar = (): JSX.Element => {
+    const daysInMonth = (iMonth: number, iYear: number) => {
+        return 32 - new Date(iYear, iMonth, 32).getDate()
+    }
+
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const firstDay = new Date(year, 3).getDay()
+    const days = daysInMonth(month, year)
+    console.log(firstDay)
     return <button>Hi</button>
 }
 
